717db4503b21384b34d8481bf7d925d3e60063ae
Mauro Soto <69121469+maurosotok@users.noreply.github.com>
2021-11-28 17:43:30 -0600

    Add files via upload

---
+++ MySQLProject.java
@@ -0,0 +1,9 @@
+package mysqlproject;
+public class MySQLProject {
+    public String username, password;
+    public static void main(String[] args) {
+        Screen x = new Screen();
+        x.setVisible(true);
+    }
+    
+}
